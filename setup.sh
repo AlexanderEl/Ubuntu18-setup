87f6bc45366e8ff6ea507422b7114f093f37ae9b
Alex Elesin <alex_elesin@sfu.ca>
2021-05-31 09:49:11 -0700

    Customizing setup script for development environment

--- setup.sh
+++ setup.sh
@@ -2,21 +2,19 @@
 
 # Ubuntu (GNOME) 18.04 setup script.
 
-dpkg -l | grep -qw gdebi || sudo apt-get install -yyq gdebi
+# dpkg -l | grep -qw gdebi || sudo apt-get install -yyq gdebi
 
 # Initial Software
 
 sudo apt update
 
-sudo apt install virtualbox virtualbox-guest-additions-iso virtualbox-ext-pack \
-net-tools htop lame git mc flatpak audacity \
-openssh-server sshfs gedit-plugin-text-size simplescreenrecorder nano \
-ubuntu-restricted-extras mpv vlc gthumb gnome-tweaks \
-gnome-tweak-tool qt5-style-plugins spell synaptic -yy
+sudo apt instal htop git vim nano gnome-tweaks terminator bpytop rofi zip unzip \
+    software-properties-common apt-transport-https wget \
+    python3-pip build-essential libssl-dev libffi-dev python3-dev python3-venv -yy
 
 # Add me to any groups I might need to be a part of:
 
-sudo adduser $USER vboxusers
+# sudo adduser $USER vboxusers
 
 # Remove undesirable packages:
 
@@ -26,21 +24,11 @@ sudo apt purge gstreamer1.0-fluendo-mp3 deja-dup shotwell whoopsie whoopsie-pref
 
 sudo snap remove gnome-characters gnome-calculator gnome-system-monitor
 sudo apt install gnome-characters gnome-calculator gnome-system-monitor \
-gnome-software-plugin-flatpak -yy
 
-# Purge Firefox, install Chromium:
-
-sudo apt purge firefox -yy
-sudo apt purge firefox-locale-en -yy
-if [ -d "/home/$USER/.mozilla" ]; then
-    rm -rf /home/$USER/.mozilla
-fi
-if [ -d "/home/$USER/.cache/mozilla" ]; then
-    rm -rf /home/$USER/.cache/mozilla
-fi
-
+# Install Chromium:
 sudo apt install chromium-browser
 
+## Disable auto updates
 gsettings set com.ubuntu.update-notifier show-livepatch-status-icon false
 #set icons to minimize on click
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
@@ -48,22 +36,22 @@ gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 ## Remove junk
 sudo apt-get remove thunderbird rhythmbox ubuntu-web-launchers -y
 
-## Multimedia
-sudo apt-get install -y gimp scribus
+## Install VSCode
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code
 
-## Games
-sudo apt-get install -y steam-installer
+## Install Atom
+wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main"
+sudo apt install atom
 
-## Music
-curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update && sudo apt-get install spotify-client -y
-
-## Text Editor
-sudo snap install --classic code
-
-## Disable Apport
+## Disable Apport (To stop sending report data)
 sudo sed -i 's/enabled=1/enabled=0/g' /etc/default/apport
+
+## Install system backup tool
+sudo add-apt-repository -y ppa:teejee2008/ppa
+sudo apt install timeshift
 
 # Gotta reboot now:
 sudo apt update && sudo apt upgrade -y
